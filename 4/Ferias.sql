2b764a679b57b5120db206646a9546903e867cc7
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-03-24 14:33:57 -0300

    update and move ferias.sql
    adicionada a relação das férias com o colaborador;
    
    ferias movida para a pasta 4.

--- 4/Ferias.sql
+++ 4/Ferias.sql
@@ -1,14 +1,16 @@
-/* create - 1*/
+/* create - 4*/
 
 create table ferias (
 	                   id serial primary key,
+		   id_colaborador int not null,
 	     data_solicitacao date not null,
 	       data_aprovacao date,
 	          data_inicio date,
 	             data_fim date,
 	quantidade_dias_abono int,
 	      fl_antecipar_13 varchar(13),
-	        justificativa varchar(100)
+	        justificativa varchar(100),
+	foreign key(id_colaborador) references colaborador(id)
 );
 
 select * from ferias;
@@ -16,6 +18,7 @@ select * from ferias;
 drop table ferias;
 
 insert into ferias (
+	id_colaborador,
 	data_solicitacao,
 	data_aprovacao,
 	data_inicio,
@@ -25,9 +28,9 @@ insert into ferias (
 	justificativa
 )
 values
-	('2020-09-15', '2021-03-20', '2021-04-01', '2021-05-01', 30, null, 'trabalha demais'),
-	('2021-02-20', '2021-03-20', '2021-05-01', '2021-06-01', 30, null, 'trabalha demais'),
-	('2021-01-25', '2021-03-20', '2021-06-01', '2021-07-01', 30, null, 'trabalha demais')
+	(5, '2020-09-15', '2021-03-20', '2021-04-01', '2021-05-01', 30, null, 'trabalha demais'),
+	(1, '2021-02-20', '2021-03-20', '2021-05-01', '2021-06-01', 30, null, 'trabalha demais'),
+	(7, '2021-01-25', '2021-03-20', '2021-06-01', '2021-07-01', 30, null, 'trabalha demais')
 ;
 
 
