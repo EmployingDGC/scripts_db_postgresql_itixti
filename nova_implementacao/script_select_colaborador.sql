b063cda2c3ca9427611e7e8561bb4d65b8e474a0
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-04-05 14:23:53 -0300

    Update script_select_colaborador.sql
    concluída as alterações sugeridas

--- nova_implementacao/script_select_colaborador.sql
+++ nova_implementacao/script_select_colaborador.sql
@@ -1,84 +1,323 @@
---faltou nos campos de ticket/contrato/cargo quero que retornem o nome do tipo
 select
-    *
+    c.id_colaborador,
+    c.nm_colaborador,
+    c.cd_cpf_cnpj,
+    c.dt_admissao,
+    c.vl_salario,
+    c2.nm_cargo,
+    tc.nm_tipo_contrato,
+    tt.nm_tipo_ticket,
+    c.dt_nascimento,
+   	c.cd_uf_natural,
+   	c.cd_etinia,
+   	c.nm_cidade_natural,
+   	c.cd_genero,
+   	c.cd_estado_civil,
+   	c.nm_mae,
+   	c.nm_pai,
+   	c.fl_cadastro_completo
 from
     colaborador c
+inner join
+	tipo_ticket tt
+on
+	c.id_ticket = tt.id_tipo_ticket
+inner join
+	tipo_contrato tc
+on
+	c.id_tipo_contrato = tc.id_tipo_contrato
+inner join
+	cargo c2
+on
+	c.id_cargo = c2.id_cargo
 order by
-    dt_admissao
-;
-
-select
-    *
-from
-    colaborador c
-order by
-    dt_nascimento
-;
-
-select
-    *
-from
-    colaborador c
-where
-    fl_cadastro_completo = true
-;
-
-select
-    *
-from
-    colaborador c
-where
-    fl_cadastro_completo = false
+    c.dt_admissao
 ;
 
 select
     c.id_colaborador,
-    tt.nm_tipo_ticket
+    c.nm_colaborador,
+    c.cd_cpf_cnpj,
+    c.dt_admissao,
+    c.vl_salario,
+    c2.nm_cargo,
+    tc.nm_tipo_contrato,
+    tt.nm_tipo_ticket,
+    c.dt_nascimento,
+   	c.cd_uf_natural,
+   	c.cd_etinia,
+   	c.nm_cidade_natural,
+   	c.cd_genero,
+   	c.cd_estado_civil,
+   	c.nm_mae,
+   	c.nm_pai,
+   	c.fl_cadastro_completo
 from
     colaborador c
 inner join
-    tipo_ticket tt
+	tipo_ticket tt
 on
-    c.id_colaborador = tt.id_tipo_ticket
+	c.id_ticket = tt.id_tipo_ticket
+inner join
+	tipo_contrato tc
+on
+	c.id_tipo_contrato = tc.id_tipo_contrato
+inner join
+	cargo c2
+on
+	c.id_cargo = c2.id_cargo
+order by
+    c.dt_nascimento 
+;
+
+select
+    c.id_colaborador,
+    c.nm_colaborador,
+    c.cd_cpf_cnpj,
+    c.dt_admissao,
+    c.vl_salario,
+    c2.nm_cargo,
+    tc.nm_tipo_contrato,
+    tt.nm_tipo_ticket,
+    c.dt_nascimento,
+   	c.cd_uf_natural,
+   	c.cd_etinia,
+   	c.nm_cidade_natural,
+   	c.cd_genero,
+   	c.cd_estado_civil,
+   	c.nm_mae,
+   	c.nm_pai,
+   	c.fl_cadastro_completo
+from
+    colaborador c
+inner join
+	tipo_ticket tt
+on
+	c.id_ticket = tt.id_tipo_ticket
+inner join
+	tipo_contrato tc
+on
+	c.id_tipo_contrato = tc.id_tipo_contrato
+inner join
+	cargo c2
+on
+	c.id_cargo = c2.id_cargo
+where
+    c.fl_cadastro_completo = true
+;
+
+select
+    c.id_colaborador,
+    c.nm_colaborador,
+    c.cd_cpf_cnpj,
+    c.dt_admissao,
+    c.vl_salario,
+    c2.nm_cargo,
+    tc.nm_tipo_contrato,
+    tt.nm_tipo_ticket,
+    c.dt_nascimento,
+   	c.cd_uf_natural,
+   	c.cd_etinia,
+   	c.nm_cidade_natural,
+   	c.cd_genero,
+   	c.cd_estado_civil,
+   	c.nm_mae,
+   	c.nm_pai,
+   	c.fl_cadastro_completo
+from
+    colaborador c
+inner join
+	tipo_ticket tt
+on
+	c.id_ticket = tt.id_tipo_ticket
+inner join
+	tipo_contrato tc
+on
+	c.id_tipo_contrato = tc.id_tipo_contrato
+inner join
+	cargo c2
+on
+	c.id_cargo = c2.id_cargo
+where
+    c.fl_cadastro_completo = false
+;
+
+select
+    c.id_colaborador,
+    c.nm_colaborador,
+    c.cd_cpf_cnpj,
+    c.dt_admissao,
+    c.vl_salario,
+    c2.nm_cargo,
+    tc.nm_tipo_contrato,
+    tt.nm_tipo_ticket,
+    c.dt_nascimento,
+   	c.cd_uf_natural,
+   	c.cd_etinia,
+   	c.nm_cidade_natural,
+   	c.cd_genero,
+   	c.cd_estado_civil,
+   	c.nm_mae,
+   	c.nm_pai,
+   	c.fl_cadastro_completo
+from
+    colaborador c
+inner join
+	tipo_ticket tt
+on
+	c.id_ticket = tt.id_tipo_ticket
+inner join
+	tipo_contrato tc
+on
+	c.id_tipo_contrato = tc.id_tipo_contrato
+inner join
+	cargo c2
+on
+	c.id_cargo = c2.id_cargo
 where
     tt.id_tipo_ticket = 1
 ;
 
 select
     c.id_colaborador,
-    c2.nm_cargo
+    c.nm_colaborador,
+    c.cd_cpf_cnpj,
+    c.dt_admissao,
+    c.vl_salario,
+    c2.nm_cargo,
+    tc.nm_tipo_contrato,
+    tt.nm_tipo_ticket,
+    c.dt_nascimento,
+   	c.cd_uf_natural,
+   	c.cd_etinia,
+   	c.nm_cidade_natural,
+   	c.cd_genero,
+   	c.cd_estado_civil,
+   	c.nm_mae,
+   	c.nm_pai,
+   	c.fl_cadastro_completo
 from
     colaborador c
 inner join
-    cargo c2
+	tipo_ticket tt
 on
-    c.id_colaborador = c2.id_cargo
+	c.id_ticket = tt.id_tipo_ticket
+inner join
+	tipo_contrato tc
+on
+	c.id_tipo_contrato = tc.id_tipo_contrato
+inner join
+	cargo c2
+on
+	c.id_cargo = c2.id_cargo
 where
     c2.id_cargo = 2
 ;
 
 select
-    *
+    c.id_colaborador,
+    c.nm_colaborador,
+    c.cd_cpf_cnpj,
+    c.dt_admissao,
+    c.vl_salario,
+    c2.nm_cargo,
+    tc.nm_tipo_contrato,
+    tt.nm_tipo_ticket,
+    c.dt_nascimento,
+   	c.cd_uf_natural,
+   	c.cd_etinia,
+   	c.nm_cidade_natural,
+   	c.cd_genero,
+   	c.cd_estado_civil,
+   	c.nm_mae,
+   	c.nm_pai,
+   	c.fl_cadastro_completo
 from
     colaborador c
+inner join
+	tipo_ticket tt
+on
+	c.id_ticket = tt.id_tipo_ticket
+inner join
+	tipo_contrato tc
+on
+	c.id_tipo_contrato = tc.id_tipo_contrato
+inner join
+	cargo c2
+on
+	c.id_cargo = c2.id_cargo
 where
-    nm_colaborador like '%ia%'
+    c.nm_colaborador like '%ia%'
 ;
 
 select
-    *
+    c.id_colaborador,
+    c.nm_colaborador,
+    c.cd_cpf_cnpj,
+    c.dt_admissao,
+    c.vl_salario,
+    c2.nm_cargo,
+    tc.nm_tipo_contrato,
+    tt.nm_tipo_ticket,
+    c.dt_nascimento,
+   	c.cd_uf_natural,
+   	c.cd_etinia,
+   	c.nm_cidade_natural,
+   	c.cd_genero,
+   	c.cd_estado_civil,
+   	c.nm_mae,
+   	c.nm_pai,
+   	c.fl_cadastro_completo
 from
     colaborador c
+inner join
+	tipo_ticket tt
+on
+	c.id_ticket = tt.id_tipo_ticket
+inner join
+	tipo_contrato tc
+on
+	c.id_tipo_contrato = tc.id_tipo_contrato
+inner join
+	cargo c2
+on
+	c.id_cargo = c2.id_cargo
 where
     vl_salario > 1000
 ;
 
 select
-    nm_dependete,
-    ds_relacao_parental
+    c.id_colaborador,
+    c.nm_colaborador,
+    c.cd_cpf_cnpj,
+    c.dt_admissao,
+    c.vl_salario,
+    c2.nm_cargo,
+    tc.nm_tipo_contrato,
+    tt.nm_tipo_ticket,
+    c.dt_nascimento,
+   	c.cd_uf_natural,
+   	c.cd_etinia,
+   	c.nm_cidade_natural,
+   	c.cd_genero,
+   	c.cd_estado_civil,
+   	c.nm_mae,
+   	c.nm_pai,
+   	c.fl_cadastro_completo
 from
-    colaborador_dependente cd
+    colaborador c
+inner join
+	tipo_ticket tt
+on
+	c.id_ticket = tt.id_tipo_ticket
+inner join
+	tipo_contrato tc
+on
+	c.id_tipo_contrato = tc.id_tipo_contrato
+inner join
+	cargo c2
+on
+	c.id_cargo = c2.id_cargo
 where
     id_colaborador = 1
 ;
-
